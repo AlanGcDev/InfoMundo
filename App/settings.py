26275e30a4b718d03576fff8296a0dbe773cd5fb
AlanGcDev <gamebrocoli@gmail.com>
2024-10-08 12:10:29 -0600

    true

--- App/settings.py
+++ App/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=@@x(kh$clk(@nd1hghr^6^m1*e4xirn(%oajfqh3ah7jmch6t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
 
