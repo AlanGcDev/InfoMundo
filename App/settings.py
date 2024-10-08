850c5d24344a3f3f469381246d2e5f5d98cb0981
AlanGcDev <gamebrocoli@gmail.com>
2024-10-08 12:43:30 -0600

    sad

--- App/settings.py
+++ App/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Asegúrate de tener esta línea
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,12 +84,15 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Media files
-MEDIA_URL = 'https://infomundo.vercel.app/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- build_files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Crear la carpeta de staticfiles si no existe
-mkdir -p staticfiles
-# Colectar los archivos estáticos
-python3 manage.py collectstatic --noinput
